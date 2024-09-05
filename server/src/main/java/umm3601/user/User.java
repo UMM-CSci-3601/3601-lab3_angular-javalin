0a03b65c5fe247a317bb82feabb875ef92118ce7
Nic McPhee <mcphee@morris.umn.edu>
2024-09-05 15:57:46 -0500

    Add a `User::toString()` method
    This can be helpful in debugging.

--- server/src/main/java/umm3601/user/User.java
+++ server/src/main/java/umm3601/user/User.java
@@ -29,4 +29,18 @@ public class User {
   public String email;
   public String avatar;
   public String role;
+
+  // Having some kind of `toString()` allows us to print `User`s,
+  // which can be useful/necessary in error handling. This only
+  // returns the name, but it could be extended to return more or
+  // all of the fields combined into a single string.
+  //
+  // The other option would be to return `_id`, but that can be
+  // `null` if we're trying to add a new `User` to the database
+  // that doesn't yet have an `_id`, so returning `name` seemed
+  // the better bet.
+  @Override
+  public String toString() {
+    return name;
+  }
 }
